f586dd7781e4e96a45a49616e030c38894adaf0a
Giorgio Alessandro Motta <cavatrendy@hotmail.com>
2020-12-30 17:26:23 +0100

    using flask

--- Main.py
+++ Main.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 
 
 @app.route("/")
+# def index():
+#     return '<h1>Hello world!</h1>'
 def hello():
     return render_template('home.html', feed_dict=feed_dict)
+
+if __name__ == '__main__':
+    app.run(debug=True)

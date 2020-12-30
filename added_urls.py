ca3b78873e08041eb6c9ae69d49800fd982292e1
Giorgio Alessandro Motta <cavatrendy@hotmail.com>
2020-12-30 15:17:56 +0100

    new file

---
+++ added_urls.py
@@ -0,0 +1,21 @@
+added_urls = ["https://cryptohackers.party/feed/rss/", "https://cointelegraph.com/rss-feeds",
+              "https://www.coindesk.com/feed/rss", "https://www.ccn.com/feed/",
+              "https://nakamotoinstitute.org/mempool/feed/",
+              "https://www.china-files.com/feed/rss", "https://agfundernews.com/feed/rss"]
+user_urls = []
+# # http://www.reddit.com/r/``{SUBREDDIT_NAME}``/new/.rss?sort=new
+# for scanned_rules in added_urls:
+#     if scanned_rules.startswith('http://www.reddit.com/'):
+#         user = input("[N]ew or [H]ot: ")
+#         if user == 'n':
+#             new = scanned_rules + '/new/.rss?sort=new'
+#             # print(new)
+#             user_urls.append(new)
+#         elif user == 'h':
+#             hot = scanned_rules + '/hot/.rss?sort=hot'
+#             # print(hot)
+#             user_urls.append(hot)
+#     else:
+#         user_urls.append(scanned_rules)
+
+# print(user_urls)

5823df51ad5cca0cfa27d198bdb714619e9b4e53
Giorgio Alessandro Motta <cavatrendy@hotmail.com>
2020-12-30 17:03:23 +0100

    djaongo

--- Main.py
+++ Main.py
@@ -1,3 +1,11 @@
+from flask import Flask, render_template
 import rss
 
-print(rss.topics)
+feed_dict = rss.files
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return render_template('home.html', feed_dict=feed_dict)

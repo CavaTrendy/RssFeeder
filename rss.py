ff9f96d49edc2f62f68932b086e3c26209059e63
Giorgio Alessandro Motta <cavatrendy@hotmail.com>
2020-12-30 15:17:40 +0100

    new mod

--- rss.py
+++ rss.py
@@ -1,53 +1,46 @@
 import feedparser
 import json
+from added_urls import added_urls
 
-added_urls = ["http://www.reddit.com/r/learnphyton+Python+learnprogramming+programming+swift",
-              "https://www.china-files.com/feed/", "https://agfundernews.com/feed", "https://www.coindesk.com/feed"]
-links = []
-# http://www.reddit.com/r/``{SUBREDDIT_NAME}``/new/.rss?sort=new
-for urls in added_urls:
-    if urls.startswith('http://www.reddit.com/'):
-        user = input("[N]ew or [H]ot: ")
-        if user == 'n':
-            new = urls + '/new/.rss?sort=new'
-            print(new)
-            links.append(new)
-        elif user == 'h':
-            hot = urls + '/hot/.rss?sort=hot'
-            print(hot)
-            links.append(hot)
-    else:
-        links.append(urls)
-
-print(links)
+links = added_urls
 
 
 class RSSFeed:
-    def __init__(self, link):
-        self.link = link
-        print("Analyzing " + self.link)
+    def __init__(self, urls):
+        self.urls = urls
+        print("Analyzing " + self.urls)
 
     def feed(self):
-        rss_feed = feedparser.parse(self.link)
+        rss_feed = feedparser.parse(self.urls)
         feed_entries = rss_feed.entries
-        feeds = {"header": [], "link": []}
+        feeds = {"header": [], "link": [], "date": []}
         for entry in feed_entries:
             feeds["header"].append(entry.title)
             feeds["link"].append(entry.link)
+            try:
+                feeds["date"].append(entry.published)
+            except:
+                print("no pubDate")
         return feeds
 
 
 topics_dict = {"title": [],
-               "url": []}
+               "date": [],
+               "url": []
+               }
 for link in links:
-    # print(RSSFeed(link).feed())
     for values in RSSFeed(link).feed().values():
+        # print(values)
         for keys in values:
+            # print(keys)
             if "htt" in keys:
                 topics_dict["url"].append(keys)
+            elif len(keys) == 31:
+                topics_dict["date"].append(keys)
             else:
                 topics_dict["title"].append(keys)
 
-new_data = [{"title": i, "url": b} for i, b in zip(topics_dict["title"], topics_dict["url"])]
+new_data = [{"title": t, "date": u, "url": i} for t, u, i in
+            zip(topics_dict["title"], topics_dict["date"], topics_dict["url"])]
 files = json.dumps(new_data, indent=3)
 print(files)

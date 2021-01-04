377046909d92685ab3b7475e2cd81e8f5e3077be
Giorgio Alessandro Motta <cavatrendy@hotmail.com>
2021-01-04 22:41:50 +0100

    controllo se ci sono nuovi feeds

--- rss.py
+++ rss.py
@@ -1,73 +1,77 @@
 import feedparser
 import json
-from url_check import added_urls
+from url_check import urls
 import re
 
-links = added_urls
+links = urls
 
 
 #######CLASS METHOD TRY##################
-# class RSSFeed:
-#     def __init__(self, urls):
-#         self.urls = urls
-#         print("Analyzing " + self.urls)
-#
-#     def feed(self):
-#         rss_feed = feedparser.parse(self.urls)
-#         feed_entries = rss_feed.entries
-#         feeds = {"header": [], "link": [], "date": []}
-#         for entry in feed_entries:
-#             feeds["header"].append(entry.title)
-#             feeds["link"].append(entry.link)
-#             try:
-#                 feeds["date"].append(entry.published)
-#             except KeyError:
-#                 print("no pubDate")
-#         return feeds
-# topics_dict = {"title": [],
-#                # "summary": [],
-#                "date": [],
-#                "url": []
-#                }
-# for link in links:
-#     for values in RSSFeed(link).feed().values():
-#         # print(values)
-#         for keys in values:
-#             # print(keys)
-#             if "htt" in keys:
-#                 topics_dict["url"].append(keys)
-#             elif len(keys) == 31 and 21:
-#                 topics_dict["date"].append(keys)
-#             else:
-#                 topics_dict["title"].append(keys)
+class RSSFeed:
+    def __init__(self, urls):
+        self.urls = urls
+        print("Analyzing " + self.urls)
 
-#########DEF METHOD ##########
-def feed():
-    for urls in links:
-        print("Analyzing " + urls)
-        rss_feed = feedparser.parse(urls)
+    def feed(self):
+        rss_feed = feedparser.parse(self.urls)
         feed_entries = rss_feed.entries
-        fedds_articles = []
-        sourcerex = re.match("https?://([A-Za-z_0-9.-]+).", urls)
-        source = sourcerex[0]
-
+        feeds = {"header": [], "link": [], "date": []}
         for entry in feed_entries:
-            header = entry.title
-            link = entry.link
+            feeds["header"].append(entry.title)
+            feeds["link"].append(entry.link)
             try:
-                date = entry.published
+                feeds["date"].append(entry.published)
             except KeyError:
                 print("no pubDate")
+        return feeds
+topics_dict = {
+    # "source": [],
+               "title": [],
+               "date": [],
+               "url": []
+               }
+for link in links:
+    # sourcerex = re.match("https?://([A-Za-z_0-9.-]+).", links)
+    # source = sourcerex[0]
+    # topics_dict["source"].append(source)
+    for values in RSSFeed(link).feed().values():
+        # print(values)
+        for keys in values:
+            # print(keys)
+            if "htt" in keys:
+                topics_dict["url"].append(keys)
+            elif len(keys) == 31 and 21:
+                topics_dict["date"].append(keys)
+            else:
+                topics_dict["title"].append(keys)
 
-            feeds = {"source": source, "header": header, "link": link, "date": date}
-            fedds_articles.append(feeds)
-    return fedds_articles
-
-
-a = feed()
+#########DEF METHOD ##########
+# def feed():
+#     for urls in links:
+#         print("Analyzing " + urls)
+#         rss_feed = feedparser.parse(urls)
+#         feed_entries = rss_feed.entries
+#         fedds_articles = []
+#         sourcerex = re.match("https?://([A-Za-z_0-9.-]+).", urls)
+#         source = sourcerex[0]
+#
+#         for entry in feed_entries:
+#             header = entry.title
+#             link = entry.link
+#             try:
+#                 date = entry.published
+#             except KeyError:
+#                 print("no pubDate")
+#
+#             feeds = {"source": source, "header": header, "link": link, "date": date}
+#             fedds_articles.append(feeds)
+#     return fedds_articles
+#
+#
+# a = feed()
 
 # print(a)
-# new_data = [{"title": t, "date": u, "url": i} for t, u, i in
-#             zip(topics_dict["title"], topics_dict["date"], topics_dict["url"])]
+new_data = [{"title": t, "date": u, "url": i} for t, u, i in
+            zip(topics_dict["title"], topics_dict["date"], topics_dict["url"])]
 with open("static/feeds.json", "w") as write_file:
-    json.dump(a, write_file, indent=4, separators=(", ", ": "), sort_keys=True)
+    json.dump(new_data, write_file, indent=3, separators=(", ", ": "), sort_keys=True)
--- url_check.py
+++ url_check.py
@@ -8,70 +8,70 @@ urls = ["https://www.cryptohackers.party/feed/rss/", "https://cointelegraph.com/
         "https://www.coindesk.com/feed/rss", "https://www.ccn.com/feed/",
         "https://www.nakamotoinstitute.org/mempool/feed/",
         "https://www.china-files.com/feed/rss", "https://agfundernews.com/feed/rss"]
-added_urls = []
-
-#### NOT ALWAYS VALID ###
-def feed_modified_date(feed):
-    # this is the last-modified value in the response header
-    # do not confuse this with the time that is in each feed as the server
-    # may be using a different timezone for last-resposne headers than it
-    # uses for the publish date
-
-    modified = feed.get('modified')
-    if modified is not None:
-        return modified
-
-    return None
-
-
-def max_entry_date(feed):
-    entry_pub_dates = (e.get('published_parsed') for e in feed.entries)
-    entry_pub_dates = tuple(e for e in entry_pub_dates if e is not None)
-
-    if len(entry_pub_dates) > 0:
-        return max(entry_pub_dates)
-
-    return None
-
-
-def entries_with_dates_after(feed, date):
-    response = []
-
-    for entry in feed.entries:
-        if entry.get('published_parsed') > date:
-            response.append(entry)
-
-    return response
-
-
-for feed_url in urls:
-    print('--------%s-------' % feed_url)
-    d = feedparser.parse(feed_url)
-    print('feed length %i' % len(d.entries))
-
-    if len(d.entries) > 0:
-        etag = d.feed.get('etag', None)
-        modified = feed_modified_date(d)
-        print('modified at %s' % modified)
-
-        d2 = feedparser.parse(feed_url, etag=etag, modified=modified)
-        added_urls.append(feed_url)
-        print('second feed length %i' % len(d2.entries))
-        if len(d2.entries) > 0:
-            print("server does not support etags or there are new entries")
-            # perhaps the server does not support etags or last-modified
-            # filter entries ourself
-
-            prev_max_date = max_entry_date(d)
-
-            entries = entries_with_dates_after(d2, prev_max_date)
-
-            print('%i new entries' % len(entries))
-        else:
-            print('there are no entries')
-    else:
-
-            added_urls.append(feed_url)
-
-print(urls)
-print(added_urls)
+# added_urls = []
+#
+# #### NOT ALWAYS VALID ###
+# def feed_modified_date(feed):
+#     # this is the last-modified value in the response header
+#     # do not confuse this with the time that is in each feed as the server
+#     # may be using a different timezone for last-resposne headers than it
+#     # uses for the publish date
+#
+#     modified = feed.get('modified')
+#     if modified is not None:
+#         return modified
+#
+#     return None
+#
+#
+# def max_entry_date(feed):
+#     entry_pub_dates = (e.get('published_parsed') for e in feed.entries)
+#     entry_pub_dates = tuple(e for e in entry_pub_dates if e is not None)
+#
+#     if len(entry_pub_dates) > 0:
+#         return max(entry_pub_dates)
+#
+#     return None
+#
+#
+# def entries_with_dates_after(feed, date):
+#     response = []
+#
+#     for entry in feed.entries:
+#         if entry.get('published_parsed') > date:
+#             response.append(entry)
+#
+#     return response
+#
+#
+# for feed_url in urls:
+#     print('--------%s-------' % feed_url)
+#     d = feedparser.parse(feed_url)
+#     print('feed length %i' % len(d.entries))
+#
+#     if len(d.entries) > 0:
+#         etag = d.feed.get('etag', None)
+#         modified = feed_modified_date(d)
+#         print('modified at %s' % modified)
+#
+#         d2 = feedparser.parse(feed_url, etag=etag, modified=modified)
+#         added_urls.append(feed_url)
+#         print('second feed length %i' % len(d2.entries))
+#         if len(d2.entries) > 0:
+#             print("server does not support etags or there are new entries")
+#             # perhaps the server does not support etags or last-modified
+#             # filter entries ourself
+#
+#             prev_max_date = max_entry_date(d)
+#
+#             entries = entries_with_dates_after(d2, prev_max_date)
+#
+#             print('%i new entries' % len(entries))
+#         else:
+#             print('there are no entries')
+#     else:
+#
+#             added_urls.append(feed_url)
+#
+# print(urls)
+# print(added_urls)

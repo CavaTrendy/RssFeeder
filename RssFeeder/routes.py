80439e823be13bc77345ac15a6231f3fd9350c63
Giorgio Alessandro Motta <cavatrendy@hotmail.com>
2021-01-08 23:13:43 +0100

    aggiunta db

---
+++ RssFeeder/routes.py
@@ -0,0 +1,20 @@
+import os
+import json
+import rss
+from models import Post
+
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/rss")
+def rss():
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "static", "feeds.json")
+    data = json.load(open(json_url))
+    page = request.args.get("page", 1, type = int)
+    posts = data.paginate(page=page, per_page=5)
+    
+    return render_template("rss.html", data=posts)

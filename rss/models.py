583ea9e119c5d16bea74840d0457b27a131b8740
Giorgio Alessandro Motta <cavatrendy@hotmail.com>
2021-01-11 10:42:00 +0100

    aggiornamento

--- rss/models.py
+++ rss/models.py
@@ -1,12 +1,13 @@
-from main import db
 from datetime import datetime
+from rss import db
 
-class Post (db.Model):
-    id = db.Column(db.Integer,primary_key = True)
-    source = db.Column(db.Text, nullable = False)
-    title = db.Column(db.Text, nullable = False)
-    date = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
-    link = db.Column(db.String , nullable = False)
-    
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    source = db.Column(db.Text, nullable=False)
+    title = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    link = db.Column(db.String, nullable=False)
+
     def __repr__(self):
         return print("Post('{self.title}', '{self.date_posted}')")
--- rss/routes.py
+++ rss/routes.py
@@ -1,10 +1,8 @@
-# import os
-# import json
+import os
+import json
 # import rss
-from flask import  render_template, request
+from flask import render_template, request
 from rss import app
-from rss.models import Post
-from rss import db
 
 
 
@@ -12,12 +10,13 @@ from rss import db
 def home():
     return render_template("home.html")
 
+
 @app.route("/rss")
 def rss():
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     json_url = os.path.join(SITE_ROOT, "static", "feeds.json")
     data = json.load(open(json_url))
-    page = request.args.get("page", 1, type = int)
+    page = request.args.get("page", 1, type=int)
     posts = data.paginate(page=page, per_page=5)
-    
+
     return render_template("rss.html", data=posts)

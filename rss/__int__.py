aaca46fe0dd96fb4236dc9c872005787efed02ab
Giorgio Alessandro Motta <cavatrendy@hotmail.com>
2021-02-27 11:48:45 +0100

    controllo se ci sono nuovi feeds

--- rss/__int__.py
+++ rss/__int__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///newsite.db"
 db = SQLAlchemy(app)
 
 from rss import routes
